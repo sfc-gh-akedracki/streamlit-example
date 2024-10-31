98234f87454802d0bbf4c37ab1f8fe1b64a5ed3d
Antoni Kedracki <antoni.kedracki@snowflake.com>
2024-10-31 16:20:27 +0100

    update 7

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit - v6!
+# Welcome to Streamlit - v7!
 
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
